eaf6d682da59e1c01ee7d5c219b6a1b922e9ddd5
Aaron Meche <aaronmeche18@icloud.com>
2025-02-13 11:51:30 -0600

    yaml slight updates and learning main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  	runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  	const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+  	@override
+  	Widget build(BuildContext context) {
+		return const MaterialApp(
+		home: Scaffold(
+			body: Padding(
+				padding: EdgeInsets.all(16.0),
+				child: Column(
+					crossAxisAlignment: CrossAxisAlignment.start,
+					children: [
+						Text(
+							'Hello World!',
+							style: TextStyle(
+								fontSize: 18,
+								fontWeight: FontWeight.bold,
+							),
+						),
+						Text('This is our brand new flashcard app!'),
+					],
+				),
+			),
+		),
+	);
   }
 }
