428347474ecedf045ff64bf245a3ff137ecdf03c
Aaron Meche <aaronmeche18@icloud.com>
2025-02-23 21:31:36 -0600

    Home screen with header and button grid widget

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -35,7 +35,6 @@ class HomeScreenStack extends StatelessWidget {
         HomePageHeader(),
         ButtonGridSection(label: "Popular Sets"),
         ButtonGridSection(label: "Recommended Sets"),
-        ButtonGridSection(label: "Placeholder"),
       ]
     );
   }
@@ -100,18 +99,32 @@ class ButtonGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Original (First) Grid Building Method
+    //
     return GridView.count(
       crossAxisCount: 3,      // Column Count
-      mainAxisSpacing: 12.0,  // Row Spacing (Height)
-      crossAxisSpacing: 12.0, // Column Spacing (Width)
-      shrinkWrap: true,       // Makes the grid take only the space it needs
-      physics: const NeverScrollableScrollPhysics(), // Disables GridView scrolling
+      mainAxisSpacing: 12.0,  // Row Spacing
+      crossAxisSpacing: 12.0, // Column Spacing
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
       children: [
         ButtonGridButton(),
         ButtonGridButton(),
         ButtonGridButton(),
       ],
     );
+
+    // Refined Grid Building Method
+    // return Wrap(
+    //   spacing: 12.0,    // Row Spacing
+    //   runSpacing: 12.0, // Column Spacing
+    //   alignment: WrapAlignment.spaceBetween,
+    //   children: const [
+    //     ButtonGridButton(),
+    //     ButtonGridButton(),
+    //     ButtonGridButton(),
+    //   ],
+    // );
   }
 }
 
@@ -119,15 +132,24 @@ class ButtonGrid extends StatelessWidget {
 class ButtonGridButton extends StatelessWidget {
   const ButtonGridButton({super.key});
 
+  void handleButtonClick() {
+    print("Button had been clicked");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(12.0)
-      ),
-      padding: EdgeInsets.all(12.0),
-      child: Text("Content"),
+    // InkWell is a custom-defined touch target
+    return InkWell(
+      onTap: () => handleButtonClick,
+      borderRadius: BorderRadius.circular(12.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(12.0)
+        ),
+        padding: EdgeInsets.all(12.0),
+        child: Text("Content"),
+      )
     );
   }
 }
