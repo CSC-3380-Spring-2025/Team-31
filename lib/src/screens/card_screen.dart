00b976d234ec92975d290379179062326909a38e
Nathan Wandera <96195805+pebblion@users.noreply.github.com>
2025-03-09 04:25:27 +0000

    Add files via upload

---
+++ lib/src/screens/card_screen.dart
@@ -0,0 +1,32 @@
+
+
+import 'package:flashcard_app/src/widgets/custom_page_header.dart';
+
+import '../widgets/essential.dart';
+import '../widgets/card.dart';
+
+class CardScreen extends StatelessWidget {
+  const CardScreen({super.key});
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return CustomPageStack(
+  //     children: [
+  //       CustomPageHeader(
+  //         text: "Card #1"
+  //       ),
+  //     ]
+  //   );
+  // }
+
+  @override
+  Widget build(BuildContext context){
+      return Scaffold(
+        body: Center(
+          child: GestureDetector(
+            onTap: (){ print("clicked"); },
+              child: FlashCard(text: 'Placeholder')
+        ))
+      );
+  }
+}
---
+++ lib/src/widgets/card.dart
@@ -0,0 +1,25 @@
+import './essential.dart';
+
+
+class FlashCard extends StatelessWidget{ 
+  final String text; // front of card
+
+  const FlashCard({
+    super.key,
+    required this.text
+  });
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    return Container(
+      width: 400,
+      height: 200,
+      alignment: Alignment.center,
+      color: Colors.green,
+      child: Text(text)
+    );
+  }
+
+}
