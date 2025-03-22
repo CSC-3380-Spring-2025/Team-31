776d747c97ef05da59d378e1242ccacf9effaa98
Elizabeth Odukoya <adaezeodukoya@gmail.com>
2025-03-22 18:45:50 -0500

    Add CameraScreen UI and layout

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ import 'src/widgets/essential.dart';
 import 'src/screens/home_screen.dart';
 import 'src/screens/create_set_screen.dart';
 import 'src/screens/settings_screen.dart';
+import 'package:flutter/material.dart';
+import 'src/screens/camera_screen.dart';
+import 'src/screens/voice_screen.dart';
 
 void main() {
 	runApp(const MyApp());
