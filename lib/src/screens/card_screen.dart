9ffa5a777380d89be39f9cea3759b94f4f62b3b4
pebblion <newandera@gmail.com>
2025-03-22 21:37:01 -0500

    Animated card on press to flip to other side

--- lib/src/screens/card_screen.dart
+++ lib/src/screens/card_screen.dart
@@ -4,10 +4,12 @@
 // This is a simple framework card screen.
 //
 
+
 import '../widgets/essential.dart';
 import '../widgets/card.dart';
+import 'dart:math';
 
-class CardScreen extends StatelessWidget {
+class CardScreen extends StatefulWidget {
   const CardScreen({super.key});
 
   // @override
@@ -21,16 +23,72 @@ class CardScreen extends StatelessWidget {
   //   );
   // }
 
+  // @override
+  // Widget build(BuildContext context){
+  //     return Scaffold(
+  //       body: Center(
+  //         child: GestureDetector(
+  //           onTap: (){ 
+  //             print("clicked"); 
+  //           },
+  //           child: FlashCard(text: 'Placeholder')
+  //       ))
+  //     );
+  // }
+
   @override
-  Widget build(BuildContext context){
-      return Scaffold(
-        body: Center(
-          child: GestureDetector(
-            onTap: (){ 
-              print("clicked"); 
-            },
-            child: FlashCard(text: 'Placeholder')
-        ))
-      );
+  State<CardScreen> createState() => _CardScreenState();
+}
+
+class _CardScreenState extends State<CardScreen>
+with TickerProviderStateMixin
+{
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  bool isflipped = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(duration: const Duration(seconds: 1),vsync: this)..animateTo(50);
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.linear);
   }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child:GestureDetector(
+          onTap: () => setState(() {} ),
+          child: GestureDetector(
+          onTap: () => setState(() {
+            isflipped = !isflipped;
+          }),
+          child: MatrixTransition(animation: _animation,
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: FlashCard(text: "placeholder"),
+          ),
+          onTransform: (double value) {
+            if (isflipped) {
+            return Matrix4.identity()
+            ..setEntry(3, 2, 0.004)
+            ..rotateY(pi * 2.0 * value);
+        }
+        else 
+        {return Matrix4.identity();}}
+        )
+        ),
+      )
+    )
+    );
+  }
+
+  
 }
