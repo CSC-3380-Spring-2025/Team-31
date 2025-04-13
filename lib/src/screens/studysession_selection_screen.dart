2ff4c4330cf7a2323cceb199d3de5bc1d993f7bb
Elizabeth Odukoya <eoduko1@lsu.edu>
2025-04-12 21:58:18 -0500

    Added Flashcard Study Session Screen with flip and hint functionality

--- lib/src/screens/studysession_selection_screen.dart
+++ lib/src/screens/studysession_selection_screen.dart
@@ -1,3 +1,4 @@
+import 'flashcard_studysession_screen.dart';
 import '../widgets/essential.dart';
 import '../widgets/card.dart';
 
@@ -5,8 +6,52 @@ class StudysessionSelectionScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text()),
-    )// TODO: implement build
-    throw UnimplementedError();
+      appBar: AppBar(title: Text('New Study Session')),
+      body: Column(
+        children: [
+          //Course Cards Section
+          Expanded(
+            child:ListView(
+              children: [
+                ListTile(title: Text('Chapter 1')),
+                ListTile(title: Text('Chapter 2')),
+                ListTile(title: Text('Chapter 3')),
+              ],
+            ),
+            ),
+            Divider(),
+            //Session Cards Section
+            Expanded(
+              child: ListView(
+              children: [
+                ListTile(title: Text('Chapter 2.1')),
+                ListTile(title: Text('Chapter 2.2')),
+                ListTile(title: Text('Chapter 2.3')),
+        ],
+      ),
+    ),
+    ],
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Icon(Icons.play_arrow),
+      onPressed: (){
+        Navigator.push(
+          context,
+           MaterialPageRoute(builder: (context) => FlashcardStudyScreen()),
+        );
+      },
+    ),
+    bottomNavigationBar: BottomAppBar(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          IconButton(icon: Icon(Icons.home), onPressed: () {
+            Navigator.pop(context);
+          }),
+        ],
+      ),
+    ),
+    );
   }
 }
+       
