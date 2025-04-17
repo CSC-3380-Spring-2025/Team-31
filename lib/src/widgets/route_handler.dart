8dcd397457647268151a217cc9dbaa5fa35c48f3
Aaron Meche <aaronmeche18@icloud.com>
2025-04-17 00:18:30 -0500

    Reworked routing system +

---
+++ lib/src/widgets/route_handler.dart
@@ -0,0 +1,48 @@
+//
+// navbar_item.dart
+// 
+// Navbar Item Widget
+
+// Required Imports
+import '../assets/essential.dart';
+// Screens
+import '../screens/home_screen.dart';
+import '../screens/create_set_screen.dart';
+import '../screens/settings_screen.dart';
+import '../screens/login_screen.dart';
+
+class RouteHandler {
+  final String route;
+
+  RouteHandler({
+    required this.route
+  });
+
+  static final Map<String, Widget> _routeToScreen = {
+    '/home': const HomeScreen(),
+    '/create_set': const CreateSetScreen(),
+    '/settings': const SettingsScreen(),
+    '/login': const LoginScreen(),
+  };
+
+  Widget getScreen() {
+    return _routeToScreen[route] ?? const NotFoundScreen();
+  }
+}
+
+class NotFoundScreen extends StatelessWidget {
+  const NotFoundScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Not Found')),
+      body: const Center(
+        child: Text(
+          '404: Page Not Found',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
